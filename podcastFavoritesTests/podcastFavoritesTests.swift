2e99ab3e6548d855d2b089071686ed16c7de1b8f
Radharani Ribas-Valongo <rad@Radharanis-MacBook-Pro.local>
2019-09-18 13:36:31 -0400

    initial commit

---
+++ podcastFavoritesTests/podcastFavoritesTests.swift
@@ -0,0 +1,34 @@
+//
+//  podcastFavoritesTests.swift
+//  podcastFavoritesTests
+//
+//  Created by Radharani Ribas-Valongo on 9/18/19.
+//  Copyright © 2019 Radharani Ribas-Valongo. All rights reserved.
+//
+
+import XCTest
+@testable import podcastFavorites
+
+class podcastFavoritesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
