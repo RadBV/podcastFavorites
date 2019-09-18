dabac8382365bc8c728a40ebacae7e2bdc20be17
Radharani Ribas-Valongo <rad@Radharanis-MacBook-Pro.local>
2019-09-18 14:53:27 -0400

    connects first tab view with view controller and creates outlets to collection view and searchbar

--- podcastFavorites/Controllers/ViewController.swift
+++ podcastFavorites/Controllers/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    //MARK: -- Outlets
+    @IBOutlet weak var podcastSearchBar: UISearchBar!
+    @IBOutlet weak var podcastCollectionView: UICollectionView!
+    
+    //MARK: -- Functions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
