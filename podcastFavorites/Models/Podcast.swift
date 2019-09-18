84d7123495790de8c7e3c8e375c1094644376c90
Radharani Ribas-Valongo <rad@Radharanis-MacBook-Pro.local>
2019-09-18 14:49:30 -0400

    adds custom collection view cell model & creates outlets to cell labels (that i also just added)

--- podcastFavorites/Models/Podcast.swift
+++ podcastFavorites/Models/Podcast.swift
@@ -9,5 +9,11 @@
 import Foundation
 
 struct Podcast: Codable {
-    
+    let results: [PodcastWrapper]
+}
+
+struct PodcastWrapper: Codable {
+    let trackId: Int
+    let artistName: String
+    let trackName: String
 }
---
+++ podcastFavorites/Models/PodcastCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  PodcastCollectionViewCell.swift
+//  podcastFavorites
+//
+//  Created by Radharani Ribas-Valongo on 9/18/19.
+//  Copyright © 2019 Radharani Ribas-Valongo. All rights reserved.
+//
+
+import UIKit
+
+class PodcastCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var trackNameLabel: UILabel!
+    
+    
+    
+    
+}
